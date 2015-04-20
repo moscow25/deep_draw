621929dd8f45b67f136e0f943a14e965d835b2f2
nicky <nickyakovenko@gmail.com>
2015-04-20 17:09:43 +0000

    added more data, at 5k runs per point evaluation. See if it matter (before progress to better learning)

--- learning/draw_poker.py
+++ learning/draw_poker.py
@@ -84,7 +84,8 @@ else:
         return pickle.load(f, encoding=encoding)
 
 DATA_URL = '' # 'http://deeplearning.net/data/mnist/mnist.pkl.gz'
-DATA_FILENAME = '../data/40k-super_sim_full_vector.csv' # smaller data set, 5k samples per hand point 
+DATA_FILENAME = '../data/100k-super_sim_full_vector.csv' # more data, still 5k samples per point; see if improvement on more inexact data?  
+#'../data/40k-super_sim_full_vector.csv' # smaller data set, 5k samples per hand point 
 #'../data/300k_full_sim_samples.csv' # big data set, 1k samples per generated hand point
 # '../data/100k_full_sim_samples.csv' # '../data/20000_full_sim_samples.csv' # '../data/100k_full_sim_samples.csv' #'../data/40000_full_sim_samples.csv'
 # Not too much accuracy gain... in doubling the training data. And more than 2x as slow.
