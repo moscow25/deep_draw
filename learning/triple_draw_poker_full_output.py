58abddb65f8a11727e651c9a405cc38b92c7e5e2
Nikolai Yakovenko <nickyakovenko@gmail.com>
2015-08-22 15:48:08 -0400

    switch back to holdem_events training

--- learning/triple_draw_poker_full_output.py
+++ learning/triple_draw_poker_full_output.py
@@ -23,7 +23,7 @@ Use similar network... to learn triple draw poker!!
 First, need new data import functins.
 """
 
-DATA_FILENAME = '../data/100k_hands_triple_draw_events.csv' # 'deuce_events' 4M hands, of the latest model (and some human play)
+DATA_FILENAME = '../data/holdem/100k_CNN_holdem_hands.csv' # 'holdem_events' trained on actually CNN hands (at least some poker ability)
 #'../data/holdem/100k_CNN_holdem_hands.csv' # 'holdem_events' trained on actually CNN hands (at least some poker ability)
 #'../data/holdem/holdem_sim_examples_50k.csv' # 'holdem_events' Small-ish dataset of simulated Hold'em hands (heuristic stochastic model). Bets in various context, ad results.
 # '../data/holdem/500k_holdem_values.csv' # 'holdem' 500k holdem hand values. Cards, possible flop, turn and river.
@@ -42,7 +42,7 @@ DATA_FILENAME = '../data/100k_hands_triple_draw_events.csv' # 'deuce_events' 4M 
 MAX_INPUT_SIZE = 550000 # 700000 # 110000 # 120000 # 10000000 # Remove this constraint, as needed
 VALIDATION_SIZE = 50000
 TEST_SIZE = 0 # 5000
-NUM_EPOCHS = 50 # 100 # 20 # 50 # 100 # 500
+NUM_EPOCHS = 20 # 50 # 100 # 20 # 50 # 100 # 500
 BATCH_SIZE = 100 # 50 #100
 BORDER_SHAPE = "valid" # "full" = pads to prev shape "valid" = shrinks [bad for small input sizes]
 NUM_FILTERS = 24 # 16 # 32 # 16 # increases 2x at higher level
@@ -67,7 +67,7 @@ LINEAR_LOSS_FOR_MASKED_OBJECTIVE = False # True # False # True
 # If we are trainging on poker events (bets, raises and folds) instead of draw value,
 # input and output shape will be the same. But the way it's uses is totally different. 
 # NOTE: We keep shape the same... so we can use the really good "draw value" model as initialization.
-TRAINING_FORMAT = 'deuce_events' # 'holdem_events'  #'holdem' # 'deuce_events' # 'deuce' # 'video'
+TRAINING_FORMAT = 'holdem_events'  #'holdem' # 'deuce_events' # 'deuce' # 'video'
 INCLUDE_HAND_CONTEXT = True # False 17 or so extra "bits" of context. Could be set, could be zero'ed out.
 DISABLE_EVENTS_EPOCH_SWITCH = True # False # Is system stable enough, to switch to adaptive training?
 
@@ -143,7 +143,7 @@ def set_mask_at_row_from_target(row_num, output_matrix, target_matrix):
 
 # For row with output value and (sparse) matrix values, combine (output) + X (real value) [X is a learning rate]
 # NOTE: Would be better to connect this to true RL, and then can increase observation significantly.
-TARGET_ACTION_LEARNING_RATE = 0.1 # 0.05
+TARGET_ACTION_LEARNING_RATE = 0.05 # 0.1 # 0.05
 def set_values_at_row_from_target(row_num, output_rows, target_rows):
     zeros = T.zeros_like(target_rows)
     zeros_subtensor = zeros[row_num[0],:] # row in 2D matrix
