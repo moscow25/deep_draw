3a37f63a1d8e6c58e70172217f2d190046774c3b
Nikolai Yakovenko <nickyakovenko@gmail.com>
2016-01-06 06:26:01 -0500

    added command export LD_LIBRARY_PATH=.:/Users/kolya/torch/install/lib:

--- acpc_submit_directory/startme.sh
+++ acpc_submit_directory/startme.sh
@@ -6,6 +6,9 @@ HE_MODEL="holdem_conv_24_filter_xCards_xNumDraws_x0_9803_double_row_percent_basl
 CNN_MODEL="nlh_events_conv_24_filter_xCards_xCommunity_xContext_0.02_CNN_1_74_Slum_Tartanian7_etc_foldper_700k.pickle"
 CNN_OLD_MODEL="nlh_events_conv_24_filter_xCards_xCommunity_xContext_0.02_CNN_1_73_Tartanian7_restart_foldper_700k.pickle"
 
+# For some reason, this needs to be executed.
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
 # Python NLH player
 # TODO: Take localhost & port from the command line
 python nlh_acpc_player.py --address=$1 --port=$2 --holdem_model=$HE_MODEL --CNN_model=$CNN_MODEL --CNN_old_model=$CNN_OLD_MODEL
